154f2276c92866780677e9036f4b2ec3d735fb1d
Jeff Huang <jeffhuang@main-10-230-101-70.tamulink.tamu.edu>
2018-10-30 10:27:12 -0500

    clean ufo

--- ufo-predict/src/main/java/trace/NodeType.java
+++
@@ -1,9 +0,0 @@
-package trace;
-
-public enum  NodeType {
-    INIT,
-    READ, WRITE, LOCK, UNLOCK, WAIT,
-    NOTIFY, NOTIFYALL, START, JOIN, BRANCH, BB,
-    PROPERTY, ALLOC, DEALLOC, RANGE_W, RANGE_R,
-    BEGIN, END, FUNC_IN, FUNC_OUT
-}
