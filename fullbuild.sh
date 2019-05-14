1281b056356f1993ce5a5c6a464161e66e1efd3f
Andrew Chin <andrew_chin@tamu.edu>
2019-05-14 13:39:22 -0500

    use absolute paths

--- fullbuild.sh
+++ fullbuild.sh
@@ -14,23 +14,23 @@ mv ufo-rt llvm/projects/
 mkdir build && cd build
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_BUILD_TESTS=OFF -DLLVM_INCLUDE_TESTS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_ENABLE_ASSERTIONS=OFF ../llvm
 make -j8
-export PATH=$PWD/bin:$PATH
+export PATH=$UFO_HOME/build/bin:$PATH
 
 # get src for chromium 70.0.3537.0 (last build to use clang 7.0.0)
-cd UFO_HOME && mkdir chromium && cd chromium
+cd $UFO_HOME && mkdir chromium && cd chromium
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-export PATH=$PWD/depot_tools:$PATH
+export PATH=$UFO_HOME/chromium/depot_tools:$PATH
 fetch --nohooks chromium
 cd src && ./build/install-build-deps.sh
 git checkout tags/70.0.3537.0 -b v70.0.3537.0
 gclient sync
 gn gen out/ufo
-mv UFO_HOME/args.gn ./out/ufo/
-mv UFO_HOME/BUILD.gn ./build/config/compiler/
+mv $UFO_HOME/args.gn ./out/ufo/
+mv $UFO_HOME/BUILD.gn ./build/config/compiler/
 # alternatively, instead of adding to the tail of $PATH, we could copy chromium's
 # lld and its symbolic links into our ufo/build/bin directory. You should change
 # to the alternative if you have lld defined already in your path somewhere.
-export PATH=$PATH:$PWD/third_party/llvm-build/Release+Asserts/bin/
+export PATH=$PATH:$UFO_HOME/chromium/src/third_party/llvm-build/Release+Asserts/bin/
 
 # build chromium
 autoninja -C out/ufo chrome
